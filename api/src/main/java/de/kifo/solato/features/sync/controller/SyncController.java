62bf52b00f1318e1eee503bfaa6aaf229999f798
Kifo <iakiv1203@gmail.com>
2026-07-12 21:15:27 +0200

    Add sync controller and fix jwt authentication filter

---
+++ api/src/main/java/de/kifo/solato/features/sync/controller/SyncController.java
@@ -0,0 +1,31 @@
+package de.kifo.solato.features.sync.controller;
+
+import de.kifo.solato.features.auth.model.User;
+import de.kifo.solato.features.sync.dto.SyncRequest;
+import de.kifo.solato.features.sync.dto.SyncResponse;
+import de.kifo.solato.features.sync.service.SyncService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/sync")
+@RequiredArgsConstructor
+public class SyncController {
+
+    private final SyncService syncService;
+
+    @PostMapping
+    public ResponseEntity<SyncResponse> synchronize(
+            @AuthenticationPrincipal User user,
+            @Valid @RequestBody SyncRequest requestDto) {
+        Long userId = user.getId();
+
+        SyncResponse responseDto = syncService.processSync(userId, requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+}
