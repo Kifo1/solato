9cb5b637aa37471a6b59ffb5d04c6edc1f5af9cb
Kifo <iakiv1203@gmail.com>
2026-07-12 21:03:11 +0200

    Add sync repositories

---
+++ api/src/main/java/de/kifo/solato/features/sync/repository/ProjectRepository.java
@@ -0,0 +1,15 @@
+package de.kifo.solato.features.sync.repository;
+
+import de.kifo.solato.features.sync.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+
+    List<Project> findByUserIdAndUpdatedAtAfter(String userId, Instant lastSyncedAt);
+}
---
+++ api/src/main/java/de/kifo/solato/features/sync/repository/SessionRepository.java
@@ -0,0 +1,15 @@
+package de.kifo.solato.features.sync.repository;
+
+import de.kifo.solato.features.sync.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface SessionRepository extends JpaRepository<Session, UUID> {
+
+    List<Session> findByUserIdAndUpdatedAtAfter(String userId, Instant lastSyncedAt);
+}
